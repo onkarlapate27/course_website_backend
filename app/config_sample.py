7ddd540e3fe7d1178151ffcc084413990c72a6d1
Onkar Lapate <onkar.lapate040@gmail.com>
2024-08-09 18:25:57 +0530

    Updated config to access env variables

--- app/config_sample.py
+++ app/config_sample.py
@@ -1,5 +1,11 @@
+import os
+
 class Settings:
-    mongo_connection_string: str = ""
-    database_name: str = ""
+    mongodb_host_name = os.environ.get("MONGO_HOST_NAME", "localhost")
+    mongodb_port = os.environ.get("MONGO_PORT", "27017")
+    db_name = os.environ.get("DB_NAME", "course_backend")
+
+    mongo_connection_string: str = f"mongodb://{mongodb_host_name}:{mongodb_port}/"
+    database_name: str = f"{db_name}"
 
 settings = Settings()
